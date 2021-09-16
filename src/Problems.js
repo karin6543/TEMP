212f39e60cf1aef310f95ab2363256ffa481b046
Iris Shi <iris.shi@mindshareworld.com>
2021-09-16 12:25:09 -0400

    keyword search function implemented

--- src/Problems.js
+++ src/Problems.js
@@ -6,9 +6,12 @@ import UserSchedule from './UserSchedule'
 function Problems() {
     
     let [arr, setArr] = useState([])
+    let [filteredArr, setFilterArr] = useState([])
     let [res, setRes] = useState([])
     let [selectId, setId] = useState()
     const idRef = useRef()
+    const searchRef = useRef()
+    
 
     const fetchData = async ()=>{
         await axios.get('https://us-east1-algo-tracker-dev.cloudfunctions.net/getProblems').then((r)=>{
@@ -32,16 +35,48 @@ function Problems() {
             }
 
             setArr(arr)
+            setFilterArr(arr)
         }
        
     }, [res])
 
+    function handleChange(e){
+
+        let searchTerm = searchRef.current.value
+        console.log('current search term-》', searchTerm)
+        if(searchTerm !==''){
+            console.log('valid term', searchTerm)
+            setFilterArr(filterByKeyword(arr, searchTerm))
+        }
+        else{
+            console.log('empty term', searchTerm)
+            setFilterArr(arr)
+        }
+
+    }
+
+    function filterByKeyword(a, keyword){
+        return a.filter((ele)=>{return ele.title.toLowerCase().indexOf(keyword.toLowerCase())!==-1})
+    }
+
+    console.log('see the filter arr:', filteredArr)
     return (
         <div>
+            
+            <form>
+           <div onChange={handleChange}>
+               <label>Search</label>
+               <input ref={searchRef}></input>
+           </div>
+          
+          
+             <button type="submit">
+               Create</button>
+             </form>
             <UserSchedule props={selectId}/>
             <button onClick={fetchData}>Load Problems</button>
            
-            {arr.length>0? arr.map(p=> <div ><Problem title={p.title} difficulty={p.difficulty} key={p.id} url={p.url} rate={p.rate} type={p.type}/><button onClick={onClickProb} id={p.id} title={p.title}>Select</button></div>):''}
+            {filteredArr.length>0? filteredArr.map(p=> <div ><Problem title={p.title} difficulty={p.difficulty} key={p.id} url={p.url} rate={p.rate} type={p.type}/><button onClick={onClickProb} id={p.id} title={p.title}>Select</button></div>):''}
    
            
         </div>
